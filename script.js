1cb039470f7967ea8554b7c97ec303ba0cab1626
lirajoaop <joaopedroababa132@gmail.com>
2024-06-13 19:51:50 -0300

    função guessLetter

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ for (let i = 0; i < selectedWord.length; i++){
 
 document.getElementById("display-word").textContent = displayWord
 
+function guessLetter (){
+    let inputElement = document.getElementById("letter-input")
+
+    // Para checar input vazio
+    if(!inputElement.value){
+        alert('Caixa vazia. Por favor, adicione uma letra.')
+        return
+    }
+    // transformar em letra minúscula
+    let letter = inputElement.value.toLowerCase()
+
+    //Limpando o campo de input
+    inputElement.value = ''
+
+    //Checar se a letra já foi adivinhada
+    if(guessedList.includes(letter)){
+        alert('Você já adivinhou essa letra!')
+        return
+    }
+
+}
